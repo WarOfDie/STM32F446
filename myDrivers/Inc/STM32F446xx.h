a8ec73935043d18a8b638cca24f8ee91a91b5185
WarOfDie <94315325+WarOfDie@users.noreply.github.com>
2023-10-23 12:31:23 +0300

    project loaded

---
+++ myDrivers/Inc/STM32F446xx.h
@@ -0,0 +1,15 @@
+/*
+ * STM32F4xx.h
+ *
+ *  Created on: Oct 23, 2023
+ *      Author: alper
+ */
+
+#ifndef INC_STM32F446XX_H_
+#define INC_STM32F446XX_H_
+
+#define FLASH_BASE_ADDR                (0x08000000UL)
+#define SRAM1_BASE_ADDR                (0x20000000UL)
+#define SRAM2_BASE_ADDR                (2001 C000)
+
+#endif /* INC_STM32F446XX_H_ */
