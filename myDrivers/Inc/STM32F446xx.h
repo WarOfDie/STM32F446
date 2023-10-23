ffbc64389e1d596b50c2785d72ac7873eb90b987
WarOfDie <94315325+WarOfDie@users.noreply.github.com>
2023-10-23 12:49:06 +0300

    added perhipheral base adresses

--- myDrivers/Inc/STM32F446xx.h
+++ myDrivers/Inc/STM32F446xx.h
@@ -8,8 +8,25 @@
 #ifndef INC_STM32F446XX_H_
 #define INC_STM32F446XX_H_
 
-#define FLASH_BASE_ADDR                (0x08000000UL)
-#define SRAM1_BASE_ADDR                (0x20000000UL)
-#define SRAM2_BASE_ADDR                (2001 C000)
+/*
+ * MEMORY BASE ADDRESS
+ */
+
+#define FLASH_BASE_ADDR                (0x08000000UL)          /*FLASH BASE ADDRESS Capacity up to 512 KBytes*/
+#define SRAM1_BASE_ADDR                (0x20000000UL)          /*SRAM1 BASE ADDRESS 116 KBytes*/
+#define SRAM2_BASE_ADDR                (0x2001C000UL) 		   /*SRAM2 BASE ADDRESS 16 KBytes*/
+
+/*
+ * Perhipheral base adresses
+ */
+
+#define PERIPH_BASE_ADDR  			    (0x40000000UL)
+#define APB1_BASE_ADDR				    PERIPH_BASE_ADDR
+#define APB2_BASE_ADDR		            (PERIPH_BASE_ADDR + 0x00010000UL)
+#define AHB1_BASE_ADDR					(PERIPH_BASE_ADDR + 0x00020000UL)
+#define AHB2_BASE_ADDR					(PERIPH_BASE_ADDR + 0x10000000UL)
+
+
+
 
 #endif /* INC_STM32F446XX_H_ */
